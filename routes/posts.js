e9281646db3f7e819fb1c45388f597691b11a14e
unknown <>
2016-12-24 21:26:35 +0900

    modify lastpage logic

--- routes/posts.js
+++ routes/posts.js
@@ -7,32 +7,37 @@ var router = express.Router();
 
 // Index
 router.get('/', function(req, res) {
-
 	//현재 페이지
 	var curPage = req.param('curPage');
 	if (curPage == null) {
 		curPage = 1;
 	}
 
-	//skip 할 데이터 사이즈
-	var sizePerPage = 10;
-	var skipSize = (curPage-1) * sizePerPage;
-	var pageNum = 1;
-
 	Post.count({}, function(err, totalCount){
 		if(err) return res.json(err);
 
-		pageNum = Math.ceil(totalCount/sizePerPage);
+		//페이지당 출력 데이터 수
+		var sizePerPage = 10;
+		var skipSize = (curPage-1) * sizePerPage;
+		var maxPage = Math.ceil(totalCount/sizePerPage);
+
+		//한번에 출력할 페이지들
+		var pagePerGroup = 10;
+		var curGroup = Math.ceil(curPage/pagePerGroup);
+		var startPage = (curGroup - 1) * pagePerGroup + 1 ;
+		var endPage = (curGroup * pagePerGroup) > maxPage ? maxPage : (curGroup * pagePerGroup) ;
 
 		Post.find({}).sort({createdAt:-1}).skip(skipSize).limit(sizePerPage).exec(
 			function(err,posts){
 				if(err) return res.json(err);
-
+				
 				res.render('posts/index',
 					{
 						posts: posts,
-						pageNum : pageNum,
-						curPage : curPage
+						maxPage : maxPage,
+						curPage : curPage,
+						startPage : startPage,
+						endPage : endPage
 					}
 				);
 			});
@@ -67,6 +72,12 @@ router.get('/:id/edit', function(req, res) {
 
 // Create
 router.post('/', function(req, res) {
+
+	//for (var i = 0; i < 1000; i++) {
+	//	req.body.title = "게시판 테스트 데이터 : " + i;
+	//	Post.create(req.body);
+	//}
+
 	Post.create(req.body, function(err, post) {
 		if (err) res.json(err);
 		res.redirect('/posts');
