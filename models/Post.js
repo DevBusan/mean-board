792b537852c8fd64fb01885d743b8fc1a4ede66d
Oh Hyun Seok <ohshaker@gmail.com>
2017-01-05 08:32:29 +0900

    removed source that comment from master branch

--- models/Post.js
+++ models/Post.js
@@ -3,50 +3,44 @@
 var mongoose = require('mongoose');
 
 var postSchema = mongoose.Schema({
-	title: {type: String, required: true},
-	body: {type: String},
-	createdAt: {type: Date, default: Date.now},
-	updatedAt: {type: Date},
-	comment : {
-		writer : {type: String, required:true},
-		memo : {type: String, required:true},
-		email : {type: String},
-		writeDate : {type:Date, default: Date.now}
-	}
+    title: { type: String, required: true },
+    body: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date }
 });
 
 postSchema.virtual('createdDate').get(function() {
-	return getDate(this.createdAt);
+    return getDate(this.createdAt);
 });
 
 postSchema.virtual('createdTime').get(function() {
-	return getTime(this.createdAt);
+    return getTime(this.createdAt);
 });
 
 postSchema.virtual('updatedDate').get(function() {
-	return getDate(this.updatedAt);
+    return getDate(this.updatedAt);
 });
 
 postSchema.virtual('updatedTime').get(function() {
-	return getTime(this.updatedAt);
+    return getTime(this.updatedAt);
 });
 
 var Post = mongoose.model('post', postSchema);
 
 function getDate(dateObj) {
-	if (dateObj instanceof Date) {
-		return dateObj.getFullYear() + '-' + get2Digits(dateObj.getMonth()+1) + '-' + get2Digits(dateObj.getDate());
-	}
+    if (dateObj instanceof Date) {
+        return dateObj.getFullYear() + '-' + get2Digits(dateObj.getMonth() + 1) + '-' + get2Digits(dateObj.getDate());
+    }
 }
 
 function getTime(dateObj) {
-	if (dateObj instanceof Date) {
-		return get2Digits(dateObj.getHours()) + ":" + get2Digits(dateObj.getMinutes())+ ":" + get2Digits(dateObj.getSeconds());
-	}
+    if (dateObj instanceof Date) {
+        return get2Digits(dateObj.getHours()) + ":" + get2Digits(dateObj.getMinutes()) + ":" + get2Digits(dateObj.getSeconds());
+    }
 }
 
 function get2Digits(num) {
-	return ("0" + num).slice(-2);
+    return ("0" + num).slice(-2);
 }
 
 module.exports = Post;
